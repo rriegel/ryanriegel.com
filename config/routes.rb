78bfa1825a05175c908f0e4cf8ebe6776e95fa9d
Ryan Riegel <ryan@riegelmedia.com>
2026-08-18 18:26:04 -0400

    fix: rubocop violations in config/routes.rb
    - Add spaces inside array brackets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :posts, only: [:index, :show], param: :slug
-    resources :categories, only: [:index, :show], param: :slug
-    resources :tags, only: [:index, :show], param: :slug
+    resources :posts, only: [ :index, :show ], param: :slug
+    resources :categories, only: [ :index, :show ], param: :slug
+    resources :tags, only: [ :index, :show ], param: :slug
   end
 end
