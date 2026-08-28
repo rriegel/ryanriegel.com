855dc075e8fbba47fc37b3a2f6a1be9461003470
Ryan Riegel <ryan@riegelmedia.com>
2026-08-28 10:00:02 -0400

    Add ActionDispatch::Cookies to ApplicationController
    ActionController::API doesn't include cookies by default.
    Explicitly include ActionDispatch::Cookies to enable
    httpOnly cookie support for authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include Devise::Controllers::Helpers
   include ActionController::MimeResponds
+  include ActionDispatch::Cookies
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :validate_content_type, if: -> { request.post? || request.put? || request.patch? }
 
