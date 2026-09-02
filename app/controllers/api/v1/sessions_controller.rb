7e55588f33d84298732c507af2d4d88678be914d
Ryan Riegel <ryan@riegelmedia.com>
2026-09-02 11:58:10 -0400

    fixed rubocop issues

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::SessionsController < ApplicationController
   private
 
   def set_auth_cookie(token)
-    is_secure = ENV.fetch('COOKIE_SECURE', Rails.env.production?).to_s == 'true'
+    is_secure = ENV.fetch("COOKIE_SECURE", Rails.env.production?).to_s == "true"
     Rails.logger.info "[AUTH] secret_key_base present: #{Rails.application.secret_key_base.present?}"
     Rails.logger.info "[AUTH] Setting auth_token cookie: secure=#{is_secure}, httponly=true, same_site=lax, path=/"
     cookies.signed[:auth_token] = {
